bcda3c15b6a6071c9a0363a22fcbb3b4367fa6f5
tomech <109527747+tomech4@users.noreply.github.com>
2026-03-18 23:16:23 +0100

    implement Movie model

--- db/models.py
+++ db/models.py
@@ -14,3 +14,13 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
+
+    def __str__(self):
+        return f"{self.title}"
