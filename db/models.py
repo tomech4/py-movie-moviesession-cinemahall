615ca9ce15091efaf29bb64a873bf69f82b9b07d
tomech <109527747+tomech4@users.noreply.github.com>
2026-03-18 23:20:09 +0100

    implement CinemaHall model

--- db/models.py
+++ db/models.py
@@ -24,3 +24,17 @@ class Movie(models.Model):
 
     def __str__(self):
         return f"{self.title}"
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+    @property
+    def capacity(self):
+        return self.rows * self.seats_in_row
+
