cd48c6df9976d48febaa1caa23dff9d3ba9846f2
tomech <109527747+tomech4@users.noreply.github.com>
2026-03-20 13:46:44 +0100

    implement create_movie

--- services/movie.py
+++ services/movie.py
@@ -15,3 +15,19 @@ def get_movies(genres_ids: list[int] = None, actors: list[int] = None) -> QueryS
 
 def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
+
+def create_movie(
+        movie_title: str,
+        movie_description: str,
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None
+) -> Movie:
+    new_movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description
+    )
+    if genres_ids:
+        new_movie.genres.add(genres_ids)
+    if actors_ids:
+        new_movie.actors.add(actors_ids)
+    return new_movie
