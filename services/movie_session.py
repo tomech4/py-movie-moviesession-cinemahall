c153c715990160b12eb8371e8d1f1184e96d16ac
tomech <109527747+tomech4@users.noreply.github.com>
2026-03-20 19:26:44 +0100

    implement update_movie_session

--- services/movie_session.py
+++ services/movie_session.py
@@ -23,3 +23,19 @@ def get_movies_sessions(session_date: str = None) -> QuerySet:
 
 def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
+
+def update_movie_session(
+        session_id: int,
+        show_time: datetime.datetime = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None,
+) -> MovieSession:
+    movie_session = MovieSession.objects.get(id=session_id)
+    if show_time:
+        movie_session.show_time = show_time
+    if movie_id:
+        movie_session.movie = movie_id
+    if cinema_hall_id:
+        movie_session.cinema_hall = cinema_hall_id
+    movie_session.save()
+    return movie_session
