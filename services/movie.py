81880b21fe1b740d1321f16b53ded176da7baef6
tomech <109527747+tomech4@users.noreply.github.com>
2026-03-20 13:37:37 +0100

    implement get_movie_by_id

--- services/movie.py
+++ services/movie.py
@@ -13,3 +13,5 @@ def get_movies(genres_ids: list[int] = None, actors: list[int] = None) -> QueryS
         movies = movies.filter(actors__id__in=actors)
     return movies
 
+def get_movie_by_id(movie_id: int) -> Movie:
+    return Movie.objects.get(id=movie_id)
