65ba88c0b291e9870731e323f2db35d9c3eab457
tomech <109527747+tomech4@users.noreply.github.com>
2026-03-20 19:29:11 +0100

    fixing typos

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,9 +1,11 @@
 import datetime
 
+from django.db.models import QuerySet
+
 from db.models import MovieSession
 
 
-def create_movie_sessions(
+def create_movie_session(
         movie_show_time: datetime.datetime,
         movie_id: int,
         cinema_hall_id: int
